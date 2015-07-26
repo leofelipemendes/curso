bfa6a4f000b3c6f60f01c8f1aedcc0f6bea8ae21
leofelipemendes <leofelipemendes@gmail.com>
2015-07-26 14:34:26 -0300

    Initial commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+
+    public function create()
+    {
+        //
+    }
+     */
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+
+    public function edit($id)
+    {
+        //
+    }*/
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cli = Client::find($id);
+        $request = $request->all();
+        $cli->name = $request['name'];
+        $cli->responsible = $request['responsible'];
+        $cli->email = $request['email'];
+        $cli->phone = $request['phone'];
+        $cli->adress = $request['adress'];
+        $cli->obs = $request['obs'];
+        if($cli->save()){
+            return "Client updated successfully";
+        }
+        return "oh oh !!! Something is wrong in you data!";
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $cli = Client::destroy($id);
+
+    }
+}
