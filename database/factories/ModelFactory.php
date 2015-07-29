db366f6378804b41f21e4230e4456c1d8524eff4
leofelipemendes <leofelipemendes@gmail.com>
2015-07-28 21:39:05 -0300

    capitulo 1 correção de exceção

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -26,7 +26,7 @@ $factory->define(CodeProject\Entities\Client::class, function (Faker\Generator $
         'responsible' => $faker->name,
         'email' => $faker->email,
         'phone' => $faker->phoneNumber ,
-        'adress' => $faker->address,
+        'address' => $faker->address,
         'obs' => $faker->sentence(),
 
     ];
