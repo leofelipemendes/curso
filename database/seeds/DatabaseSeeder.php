e4f7d66ff9847cdb158bd7e8b781d0e2e6bb37df
leofelipemendes <leofelipemendes@gmail.com>
2015-08-18 18:32:00 -0300

    Capitulo 2 Service Layer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        DB::statement('SET foreign_key_checks = 0');
 
         $this->call(ClientTableSeeder::class);
         // $this->call(UserTableSeeder::class);
+        DB::statement('SET foreign_key_checks = 1');
 
         Model::reguard();
     }
